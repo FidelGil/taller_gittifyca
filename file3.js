ebe6e22d1ce83cf8f71e26ed0d11b769baea5835
Fidel_Gil <fgil@computer.org>
2018-09-21 18:39:02 +0000

    Código base de JavaScript

---
+++ file3.js
@@ -0,0 +1,19 @@
+//
+// Main module
+//
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall(ctx);
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 10);
+
---
+++ file4.js
@@ -0,0 +1,11 @@
+//
+// Support module
+//
+function drawBall(ctx) {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
